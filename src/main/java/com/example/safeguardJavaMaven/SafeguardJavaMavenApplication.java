07367960784ac696ae1ce6228ae38f8fe5d53b5a
Pramod Burly <pramodburly@Pramods-MacBook-Pro.local>
2022-01-03 00:10:34 -0700

    initial push

---
+++ src/main/java/com/example/safeguardJavaMaven/SafeguardJavaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.safeguardJavaMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeguardJavaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeguardJavaMavenApplication.class, args);
+	}
+
+}
